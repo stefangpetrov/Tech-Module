4f00c890197479c94ba26cf63035d8d89122169d
stefangpetrov <stef4ogpetrov@gmail.com>
2017-01-18 21:18:37 +0200

    Exercises

---
+++ Data Types and Variables Exercises/Fast Prime Checker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fast_Prime_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int final = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= final; i++)
+            {
+                bool primeornot = true;
+                for (int j = 2; j <= Math.Sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        primeornot = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{i} -> {primeornot}");
+            }
+
+        }
+    }
+}
---
+++ Data Types and Variables Exercises/Thea the Photographer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thea_the_Photographer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int allpics = int.Parse(Console.ReadLine());
+            int filtertime = int.Parse(Console.ReadLine());
+            int filterfactor = int.Parse(Console.ReadLine());
+            int uploadtime = int.Parse(Console.ReadLine());
+
+            int useful = allpics * (filterfactor/100);
+            int totaltime = allpics * filtertime + useful * uploadtime;
+
+            TimeSpan time = TimeSpan.FromSeconds(totaltime);
+            Console.WriteLine("{0:d\\:hh\\:mm\\:ss}", time);
+        }
+    }
+}
