492fd77d9cf2e433613744bf0f044e863c607a98
stefangpetrov <stef4ogpetrov@gmail.com>
2017-02-05 20:26:14 +0200

    Aide pak :D

---
+++ Lists Lab/CountNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountNumbers
+{
+    class CountNumbers
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int[] count = new int[numbers.Max() + 1];
+
+            foreach (var number in numbers)
+            {
+                count[number]++;
+
+            }
+            for (int i = 0; i < count.Length; i++)
+            {
+                if(count[i] >= 1)
+                    Console.WriteLine($"{i} -> {count[i]} ");
+            }
+        }
+    }
+}
---
+++ Lists Lab/SquareNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareNumbers
+{
+    class SquareNumbers
+    {
+        static void Main()
+        {
+
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var squarenums = new List<int>(); 
+             
+            foreach(int number in numbers)
+            {
+                if(CheckSquare(number))
+                {
+                    squarenums.Add(number);
+                }
+            }
+            squarenums.Sort((a, b) => b.CompareTo(a));
+
+            Console.WriteLine(String.Join(" ", squarenums));
+        }
+
+        private static bool CheckSquare(int number)
+        {
+            if (Math.Sqrt(number) == (int)Math.Sqrt(number))
+                return true;
+            else
+                return false;
+        }
+    }
+}
